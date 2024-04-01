80e11deabcc12dcf148e210b72be1d80454c41f3
Abdibaitov <91374399+Abdibaitov@users.noreply.github.com>
2024-04-01 14:08:51 +0300

    recently undates

--- src/City.java
+++ src/City.java
@@ -1,2 +1,33 @@
 public class City {
+    String name;
+    int age;
+
+    public City(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
